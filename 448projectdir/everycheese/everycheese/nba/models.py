6468d7840c4bdb6cd657c547bfcf6ab2834764e8
Brenden Highlander <bhighlan@mail.umw.edu>
2024-04-08 12:14:18 -0400

    Added Match Model

--- 448projectdir/everycheese/everycheese/nba/models.py
+++ 448projectdir/everycheese/everycheese/nba/models.py
@@ -10,3 +10,14 @@ class Team(TimeStampedModel):
     
     def __str__(self):
         return self.team_name
+
+class Match(TimeStampedModel):
+    match_name = models.CharField("Match Name", max_length=255)
+    match_date = models.DateField("Match Date", blank=True)
+    team_id1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1', null=True)
+    team_id2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2', null=True)
+    winner_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='winner', null=True)
+    created_date = models.DateField("Date Created:", blank=True)
+    
+    def __str__(self):
+        return self.match_name
