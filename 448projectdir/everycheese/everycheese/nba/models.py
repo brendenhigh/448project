9ebed9273a61831bbd19a367c751bbe97a78fed2
Brenden Highlander <bhighlan@mail.umw.edu>
2024-04-08 11:58:24 -0400

    Added Team Model

--- 448projectdir/everycheese/everycheese/nba/models.py
+++ 448projectdir/everycheese/everycheese/nba/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+class Team(TimeStampedModel):
+    team_name = models.CharField("Team Name", max_length=255)
+    slug = AutoSlugField("Team Address", unique=True, always_update=False, populate_from="team_name")
+    created_date = models.DateField("Date Created:", blank=True)
+    
+    def __str__(self):
+        return self.team_name
