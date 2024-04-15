cb2a6b5875c3b1df60323651c0b455cb5065f5ad
Brenden Highlander <bhighlan@mail.umw.edu>
2024-04-15 08:20:06 -0400

    Added foreign key to team model and added team to user model.

--- 448projectdir/everycheese/everycheese/nba/models.py
+++ 448projectdir/everycheese/everycheese/nba/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
+from django.conf import settings
 
 class Team(TimeStampedModel):
     team_name = models.CharField("Team Name", max_length=255)
     slug = AutoSlugField("Team Address", unique=True, always_update=False, populate_from="team_name")
     created_date = models.DateField("Date Created:", blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.team_name
@@ -21,5 +23,9 @@ class Match(TimeStampedModel):
     
     def __str__(self):
         return self.match_name
+
+class User():
+    team = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
 	
