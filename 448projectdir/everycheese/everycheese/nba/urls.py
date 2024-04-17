c1d7df33135cb850f5cd7561d485cabdb71947f8
Cortega2-1 <cortega2@mail.umw.edu>
2024-04-17 10:16:05 -0400

    added matches view/url

--- 448projectdir/everycheese/everycheese/nba/urls.py
+++ 448projectdir/everycheese/everycheese/nba/urls.py
@@ -3,11 +3,21 @@ from . import views
 
 app_name = "nba"
 urlpatterns = [
+    path(
+        route='matches/', 
+        view=views.MatchListView.as_view(), 
+        name='match_list'
+    ),
 	path (
 		route='',
 		view=views.TeamListView.as_view(),
 		name='team_list'
 	),
+     path(
+        route='matches/<slug:slug>/', 
+        view=views.MatchDetailView.as_view(), 
+        name='match_detail'
+    ),
     path (
         route='<slug:slug>',
         view=views.TeamDetailView.as_view(),
--- 448projectdir/everycheese/everycheese/nba/views.py
+++ 448projectdir/everycheese/everycheese/nba/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import ListView, DetailView
 
-from .models import Team
+from .models import Team, Match
 
 class TeamListView(ListView):
 	model = Team
+
+class MatchListView(ListView):
+    model = Match
+
+class MatchDetailView(DetailView):
+    model = Match
+
 class TeamDetailView(DetailView):
 	model = Team
