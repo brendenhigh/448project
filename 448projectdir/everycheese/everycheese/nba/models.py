4773b92f93f95820a3517d9d168fa5d9cb63ba5b
Brenden Highlander <bhighlan@mail.umw.edu>
2024-04-23 15:17:59 -0400

    Fixed tables

--- 448projectdir/everycheese/everycheese/nba/models.py
+++ 448projectdir/everycheese/everycheese/nba/models.py
@@ -10,7 +10,7 @@ class Team(TimeStampedModel):
     team_name = models.CharField("Team Name", max_length=255)
     slug = AutoSlugField("Team Address", unique=True, always_update=False, populate_from="team_name")
     created_date = models.DateField("Date Created:", blank=True)
-    owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.team_name
