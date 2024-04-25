354a68279fedefbd228cbb5116d4dcd0084e0e9a
Brenden Highlander <bhighlan@mail.umw.edu>
2024-04-25 09:17:43 -0400

    Added team to admin.py in User

--- 448projectdir/everycheese/everycheese/users/admin.py
+++ 448projectdir/everycheese/everycheese/users/admin.py
@@ -16,7 +16,7 @@ class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (
-        ("User", {"fields": ("name",)}),
+        ("User", {"fields": ("name", "team")}),
     ) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
